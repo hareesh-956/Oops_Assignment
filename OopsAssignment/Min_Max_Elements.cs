6b154a7087ce9b80364b017bfa6ac8d830bb4d4a
hareesh <hareesh.vanapalli.888@gmail.com>
2023-06-19 00:28:06 +0530

    [Hareesh]

---
+++ OopsAssignment/Min_Max_Elements.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsAssignment
+{
+    internal class Min_Max_Elements
+    {
+        public static void Min_Max()
+        {
+            Console.Write("enter a number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(arr[i]);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int Max = arr[0];
+            int Min = arr[0];
+
+            if (Max > Min)
+            {
+                Max = arr[0];
+            }
+            if (Min < Max)
+            {
+                Min = arr[0];
+            }
+
+            Console.WriteLine("Maximum element: " + Max);
+            Console.WriteLine("Minimum element: " + Min);
+
+        }
+    }
+}
--- OopsAssignment/Program.cs
+++ OopsAssignment/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Please Choose any one program from below option");
-            Console.WriteLine("1.Duplicate_Element\n2.Unique_Elements\n");
+            Console.WriteLine("1.Duplicate_Element\n2.Unique_Elements\n3.Min_Max_Elements\n");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
@@ -16,6 +16,10 @@
 
                 case 2:
                     Unique_Elements.Unique();
+                    break;
+
+                case 3:
+                    Min_Max_Elements.Min_Max();
                     break;
             }
 
