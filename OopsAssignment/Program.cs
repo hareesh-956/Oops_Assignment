611314ab1c33e38300af42d6045f1695003a33af
hareesh <hareesh.vanapalli.888@gmail.com>
2023-06-18 21:21:35 +0530

    [Hareesh]

---
+++ OopsAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace OopsAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Choose any one program from below option");
+            Console.WriteLine("1.Duplicate_Element\n");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Duplicat_Element.Duplicate();
+                break;
+            }
+
+
+        }
+    }
+}
